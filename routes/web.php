2d6112af9329e0156f23fd6b0443e9681c61c6b1
Jose Lopes Nei <joselopesnei@gmail.com>
2026-04-27 21:20:53 -0300

    fix: move rota broadcast antes das rotas com {id} para evitar conflito de matching

--- routes/web.php
+++ routes/web.php
@@ -106,10 +106,10 @@ Route::middleware(['auth', 'can:access-manager'])->group(function () {
     Route::get('/prospecting', [App\Http\Controllers\ProspectingController::class, 'index'])->name('prospecting.index');
     Route::post('/prospecting/search', [App\Http\Controllers\ProspectingController::class, 'search'])->name('prospecting.search');
     Route::post('/prospecting/analyze-all', [App\Http\Controllers\ProspectingController::class, 'analyzeAll'])->name('prospecting.analyze-all');
+    Route::post('/prospecting/broadcast', [App\Http\Controllers\ProspectingController::class, 'broadcastWhatsapp'])->name('prospecting.broadcast');
     Route::post('/prospecting/{id}/analyze', [App\Http\Controllers\ProspectingController::class, 'analyze'])->name('prospecting.analyze');
     Route::post('/prospecting/{id}/convert', [App\Http\Controllers\ProspectingController::class, 'convertToDeal'])->name('prospecting.convert');
     Route::delete('/prospecting/{id}', [App\Http\Controllers\ProspectingController::class, 'destroy'])->name('prospecting.destroy');
-    Route::post('/prospecting/broadcast', [App\Http\Controllers\ProspectingController::class, 'broadcastWhatsapp'])->name('prospecting.broadcast');
 });
 
 Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->middleware(['auth', 'subscription'])->name('dashboard');
