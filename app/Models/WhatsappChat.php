ecbaabcfb866d7e794fc8313f51b939f5e985975
Jose Lopes Nei <joselopesnei@gmail.com>
2026-06-27 17:31:33 -0300

    fix: adiciona relacionamento tenant() em WhatsappChat (causa raiz do Falha ao analisar)

--- app/Models/WhatsappChat.php
+++ app/Models/WhatsappChat.php
@@ -42,6 +42,10 @@ class WhatsappChat extends Model
         'is_bot_active',
     ];
 
+    public function tenant() {
+        return $this->belongsTo(Tenant::class);
+    }
+
     public function messages() {
         return $this->hasMany(WhatsappMessage::class, 'chat_id');
     }
